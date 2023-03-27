5f928707ae4f084e2455031c3d13e09e772b3ae5
Hovanella <howansky.timofei@yandex.ru>
2023-03-27 16:43:49 +0300

    init

---
+++ 20-01.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const contactRouter = require('./routers/contactRouter');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs',
+    helpers: {
+        goBack: () => 'window.location.href = \'/\''
+    }
+});
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+const app = express();
+
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use('/', contactRouter);
+
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`http://localhost:${port}`);
+    console.log('https://laba20-express.herokuapp.com');
+});
+
+
